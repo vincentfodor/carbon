81ab3a6b908e7fea1d4380703f99b54dcafaf0c7
marcinstachowicz <mstachowicz@pgs-soft.com>
2019-07-26 10:24:51 +0200

    audit fix and add -m=4096

--- script/docker/setup.sh
+++ script/docker/setup.sh
@@ -2,6 +2,6 @@
 set -e
 
 echo 'Build Docker Image'
-docker build --rm -t carbon_cypress -f Dockerfile-cypress --memory-swap=-1 --shm-size=256M .
+docker build --rm -t carbon_cypress -f Dockerfile-cypress --memory-swap=-1 --shm-size=256M -m=4096M .
 
 echo 'Setup complete'
