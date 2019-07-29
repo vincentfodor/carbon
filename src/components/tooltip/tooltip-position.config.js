1a0afac617cc445c184e24b9c54c6a28cb2776e2
Michael Hogg <michael@kyan.com>
2019-07-29 17:24:48 +0100

    (FE-1847) Refactor tooltip-position.config.js (no functionality change)

--- src/components/tooltip/tooltip-position.config.js
+++ src/components/tooltip/tooltip-position.config.js
@@ -1,44 +1,4 @@
-import { startCase } from 'lodash';
 import { styleElement } from '../../utils/ether';
-
-/**
- * Calculates position for tooltip.
- *
- * @method calculatePosition
- * @param {Object} tooltip
- * @param {Object} target
- * @return {Object} shifts calculated
- */
-function calculatePosition(tooltip, target) {
-  const tooltipWidth = tooltip.offsetWidth,
-      tooltipHeight = tooltip.offsetHeight,
-      pointerDimension = 15,
-      // hardcode height & width since span has no dimensions
-      pointerOffset = 11,
-      targetWidth = target.offsetWidth,
-      targetHeight = target.offsetHeight,
-      targetRect = target.getBoundingClientRect(),
-      offsetY = window.pageYOffset,
-      targetTop = targetRect.top + offsetY,
-      targetBottom = targetRect.bottom + offsetY,
-      targetLeft = targetRect.left,
-      targetRight = targetRect.right;
-
-  const shifts = {
-    verticalY: targetTop - tooltipHeight - (pointerDimension * 0.5),
-    verticalBottomY: targetBottom + (pointerDimension * 0.5),
-    verticalCenter: (targetLeft - (tooltipWidth * 0.5)) + (targetWidth * 0.5),
-    verticalRight: (targetLeft + pointerDimension + pointerOffset) - tooltipWidth,
-    verticalLeft: targetLeft - (pointerDimension * 0.5),
-    rightHorizontal: targetRight + (0.5 * pointerDimension),
-    leftHorizontal: targetLeft - (pointerDimension * 0.5) - tooltipWidth,
-    sideTop: targetTop - pointerOffset,
-    sideBottom: (targetTop - tooltipHeight) + targetHeight + pointerOffset,
-    sideCenter: (targetTop + (targetHeight * 0.5)) - (tooltipHeight * 0.5)
-  };
-
-  return shifts;
-}
 
 /**
  * Update a CSS position attribute of the tooltip,
@@ -66,32 +26,52 @@ function setPositionAttributeIfChanged(tooltip, attribute, pixels) {
  * @return {Object} New coordinates of tooltip
  */
 export default function positionTooltip(tooltip, target, position, alignment) {
-  const shifts = calculatePosition(tooltip, target);
+  // Width and height of the <Tooltip> component's <div>
+  const tooltipWidth = tooltip.offsetWidth;
+  const tooltipHeight = tooltip.offsetHeight;
+
+  const pointerDimension = 15;
+  // hardcode height & width since span has no dimensions
+  const pointerOffset = 11;
+
+  // Width and height of the target element
+  const targetWidth = target.offsetWidth;
+  const targetHeight = target.offsetHeight;
+
+  // Coordinates of the target element (relative to the viewport)
+  const targetRect = target.getBoundingClientRect();
+
+  // Vertical scrolling offset of the viewport
+  const offsetY = window.pageYOffset;
+
+  // Coordinates of the target element (absolute)
+  const targetTop = targetRect.top + offsetY;
+  const targetBottom = targetRect.bottom + offsetY;
+  const targetLeft = targetRect.left;
+  const targetRight = targetRect.right;
+
+  const horizontalAlign = {
+    center: (targetLeft - (tooltipWidth * 0.5)) + (targetWidth * 0.5),
+    right: (targetLeft + pointerDimension + pointerOffset) - tooltipWidth,
+    left: targetLeft - (pointerDimension * 0.5)
+  };
+
+  const verticalAlign = {
+    top: targetTop - pointerOffset,
+    bottom: (targetTop - tooltipHeight) + targetHeight + pointerOffset,
+    center: (targetTop + (targetHeight * 0.5)) - (tooltipHeight * 0.5)
+  };
 
   let x = 0;
   let y = 0;
 
-  switch (position) {
-    case 'top':
-      x = shifts[`vertical${startCase(alignment)}`];
-      y = shifts.verticalY;
-      break;
+  if (position === 'top') y = targetTop - tooltipHeight - (pointerDimension * 0.5);
+  if (position === 'bottom') y = targetBottom + (pointerDimension * 0.5);
+  if (position === 'left') x = targetLeft - (pointerDimension * 0.5) - tooltipWidth;
+  if (position === 'right') x = targetRight + (0.5 * pointerDimension);
 
-    case 'bottom':
-      x = shifts[`vertical${startCase(alignment)}`];
-      y = shifts.verticalBottomY;
-      break;
-
-    case 'left':
-      x = shifts[`${position}Horizontal`];
-      y = shifts[`side${startCase(alignment)}`];
-      break;
-
-    case 'right':
-    default:
-      x = shifts[`${position}Horizontal`];
-      y = shifts[`side${startCase(alignment)}`];
-  }
+  if (['top', 'bottom'].includes(position)) x = horizontalAlign[alignment];
+  if (['left', 'right'].includes(position)) y = verticalAlign[alignment];
 
   setPositionAttributeIfChanged(tooltip, 'left', x);
   setPositionAttributeIfChanged(tooltip, 'top', y);
