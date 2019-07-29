2b17b7e93f8a5c4760abbee7a36a1c2948c170fc
Michael Hogg <michael@kyan.com>
2019-07-29 15:05:26 +0100

    (FE-1847) Move calculatePosition() to new file

--- src/utils/decorators/tooltip-decorator/tooltip-decorator.js
+++ src/utils/decorators/tooltip-decorator/tooltip-decorator.js
@@ -241,49 +241,6 @@ const TooltipDecorator = (ComposedComponent) => {
       return ReactDOM.findDOMNode(this._tooltip); // eslint-disable-line react/no-find-dom-node
     }
 
-    /**
-     * Calculates position for tooltip, first result cached.
-     *
-     * @method calculatePosition
-     * @param {Object} tooltip
-     * @param {Object} target
-     * @return {Object} shifts calculated
-     */
-    calculatePosition = (tooltip, target) => {
-      if (this._memoizedShifts) { return this._memoizedShifts; }
-
-      const tooltipWidth = tooltip.offsetWidth,
-          tooltipHeight = tooltip.offsetHeight,
-          pointerDimension = 15,
-          // hardcode height & width since span has no dimensions
-          pointerOffset = 11,
-          targetWidth = target.offsetWidth,
-          targetHeight = target.offsetHeight,
-          targetRect = target.getBoundingClientRect(),
-          offsetY = window.pageYOffset,
-          targetTop = targetRect.top + offsetY,
-          targetBottom = targetRect.bottom + offsetY,
-          targetLeft = targetRect.left,
-          targetRight = targetRect.right;
-
-      const shifts = {
-        verticalY: targetTop - tooltipHeight - (pointerDimension * 0.5),
-        verticalBottomY: targetBottom + (pointerDimension * 0.5),
-        verticalCenter: (targetLeft - (tooltipWidth * 0.5)) + (targetWidth * 0.5),
-        verticalRight: (targetLeft + pointerDimension + pointerOffset) - tooltipWidth,
-        verticalLeft: targetLeft - (pointerDimension * 0.5),
-        rightHorizontal: targetRight + (0.5 * pointerDimension),
-        leftHorizontal: targetLeft - (pointerDimension * 0.5) - tooltipWidth,
-        sideTop: targetTop - pointerOffset,
-        sideBottom: (targetTop - tooltipHeight) + targetHeight + pointerOffset,
-        sideCenter: (targetTop + (targetHeight * 0.5)) - (tooltipHeight * 0.5)
-      };
-
-      this.realignOffscreenTooltip(shifts, tooltipWidth);
-
-      return shifts;
-    };
-
     realignOffscreenTooltip = (shifts, tooltipWidth) => {
       if (this.props.tooltipAlign === 'right' || this.state.tooltipAlign === 'right') return;
 
